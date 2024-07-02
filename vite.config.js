bc1769c59230048937db2321ac2fb115cbd7f4ba
Siddarthan999 <siddarthan999@gmail.comm>
2024-07-02 22:09:45 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//Siddarthan - 212221040154
 export default defineConfig({
   plugins: [react()],
   base: 'Toggle-Visibility-Feature-D2-A1'
